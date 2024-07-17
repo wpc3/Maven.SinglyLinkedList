9e37ab8719902941e272bddf72ee29ce101ed179
Will <will@Zipcoders-MacBook-Pro-9.local>
2024-07-17 08:09:33 -0400

    done

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -7,7 +7,7 @@ public class SinglyLinkedList <E extends Comparable<E>> {
 
 
 
-
+//Inner class we need to make to create a node
     public class Node {
      E data;
      Node next;
@@ -25,6 +25,7 @@ public class SinglyLinkedList <E extends Comparable<E>> {
         //list size
         int size;
 
+        //Getting a method for the size of our list
     public int size() {
         return size;
     }
@@ -36,34 +37,34 @@ public class SinglyLinkedList <E extends Comparable<E>> {
    }
 
     public void add(E i) {
-       Node newNode = new Node(i);
+       Node newNode = new Node(i);//Create a new node with data "i"
        if(head == null){
-        head = newNode;
+        head = newNode;//if list is empty, head is new node
        }
        else {
-           Node current = head;
-           while (current.next != null){
+           Node current = head;//else if list is not empty, start from "Head"
+           while (current.next != null){//"Travesing" to the end of the list
                current = current.next;
            }
-    current.next = newNode;
+    current.next = newNode;//Add newNode to end of list
        }
 
-        size++;
+        size++;//Increment size of the list by each new node
     }
 
     public void remove(int index) {
        if(index < 0 || index>= size){
-           throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-       }
+           throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);//Checking if our index is out of bounds
+       }//Removing the first node
        if(index ==0){
-           head = head.next;
+           head = head.next; //Move head to next node,which will remove the current head
        }
-       else{
+       else{//Removing if index is greater than 0
            Node current = head;
-           for (int i = 0; i<index-1; i++){
+           for (int i = 0; i<index-1; i++){//Traverse to the node just before the one to be removed
                current = current.next;
            }
-           current.next = current.next.next;
+           current.next = current.next.next;//Update the currents next reference to skip ove the node we removed
        }
 
 
@@ -72,27 +73,27 @@ public class SinglyLinkedList <E extends Comparable<E>> {
 
 
     public boolean contains(E element) {
-        Node current = head;
-        while(current != null){
-            if (current.data.equals(element)){
+        Node current = head; //Start from the head of the list
+        while(current != null){//Traverse the list until you get to the end
+            if (current.data.equals(element)){//Checking if the current node's data matches the element
                 return true;
             }
-            current = current.next;
+            current = current.next; //If it doesnt, move to the next node
         }
         return false;
     }
 
     public int find(E element) {
         Node current = head;
-        int index = 0;
+        int index = 0;//index counter
         while (current != null){
-            if (current.data.equals(element)){
+            if (current.data.equals(element)){//Checking if the current node's index matches element
                 return index;
 
             }
 
-            current = current.next;
-            index++;
+            current = current.next;//Move to the next node if it doesn't
+            index++;//increment the index as you move through the nodes
         }
         return -1;
 
@@ -104,11 +105,11 @@ public class SinglyLinkedList <E extends Comparable<E>> {
         }
 
             Node current = head;
-            for (int i =0; i<index; i++){
-                current = current.next;
+            for (int i =0; i<index; i++){//Traverse through the list until we get the index we're looking for
+                current = current.next;//Moving to the next node
             }
 
-        return current.data;
+        return current.data; //Return the data of the node at the specified index
     }
 
 
@@ -124,34 +125,34 @@ public class SinglyLinkedList <E extends Comparable<E>> {
 
 
     public SinglyLinkedList<E> sort() {
-        if (size > 1) {
-            boolean swapped;
+        if (size > 1) {//Check if there is more than one element in the list
+            boolean swapped; //Flag to track if any elements were swapped
             do {
-                swapped = false;
+                swapped = false; //Initialize swapped to false to start our iteration
                 Node current = head;
-                Node previous = null;
+                Node previous = null; //Track the previous node during traversal
                 while (current.next != null) {
-                    if (current.data.compareTo(current.next.data) > 0) {
-                        Node temp = current.next;
+                    if (current.data.compareTo(current.next.data) > 0) {//Compare current and next node's data
+                        Node temp = current.next;//Swap current and next nodes
                         current.next = temp.next;
                         temp.next = current;
-                        if (previous == null) {
-                            head = temp;
+                        if (previous == null) {//Update previous node's next reference
+                            head = temp; //update the head if swapping the first element
                         } else {
-                            previous.next = temp;
+                            previous.next = temp; //Link the previous node to the new node after swapped
                         }
-                        previous = temp;
-                        swapped = true;
+                        previous = temp; //Update the previous to the swapped node
+                        swapped = true; //Set swap to true
                     } else {
-                        previous = current;
+                        previous = current; //Move previous and current pointer forward
                         current = current.next;
                     }
                 }
 
-            } while (swapped);
+            } while (swapped); //Continue swapping while swaps were made in this pass
 
         }
-        return null;
+        return null; //Set null as the placeholder
 
     }
     }
